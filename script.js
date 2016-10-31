62fedbf463161a55995bdc84da17dac54a6d8daa
chris-A-phillips <cphillips918@gmail.com>
2016-10-31 09:47:48 -0400

    add portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("hello world")
+
+var sentence ="this is a string!"
+var number = 5 + 5
+
+var counter = O
+$("h1").on("click"), function(){
+  $("h1").html(counter)
+  counter = counter + 1
+}
+
+var name = prompt("What is your name?")
+var quest = prompt("What is your quest?")
+var color = prompt("What is your favorite color")
+
+$("p")
+
+$("p.description")
+
+$("h1").html()
+$("h2").css()
+
+$("h1").on("click",funtion(){
+  console.log("h1 was clicked")
+}
+$("h1").on("click",funtion(){
+  $("h2").toggle()
